084b36818812945ae99d1927bf627330695f5434
cesarlongart101 <61634826+cesarlongart101@users.noreply.github.com>
2023-04-21 20:18:49 +0200

    Actualizacion de ejercicio 5 (#1)
    * actualizar ejercicio 5 con Branch
    
    * Cambio 2 en Branch

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -1,9 +1,11 @@
-import numpy
-r = float(input("Radio: "))
-long = 2*3.1416*r
+print("Programa para calcular area de circulo")
+radio = float(input("Radio: "))
+print()
+long = 2*3.1416*radio
 print("La longitud de circunferencia es la siguiente: ", long)
 area = 3.1416*r*r
+print("**************************")
 print("El área final es: ", area)
-
+print("**************************")
 print("TEST COMMIT", area, "valor de area")
 

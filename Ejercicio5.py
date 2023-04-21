8d0bbc04ff909316318f803d98b4092e3aef2459
cesarlongart101 <cesarlongart101@gmail.com>
2023-04-21 19:02:05 +0200

    Agregar ejercicio 5

---
+++ Ejercicio5.py
@@ -0,0 +1,7 @@
+import numpy
+r = float(input("Radio: "))
+long = 2*3.1416*r
+print("La longitud de circunferencia es: ", long)
+area = 3.1416*r*r
+print("El área de la circunferencia es: ", area)
+
